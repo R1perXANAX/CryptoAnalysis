4e497cf7e16e0c27c6a4057d3ebcd7d7148088df
R1perXNX <107111095+R1perXANAX@users.noreply.github.com>
2023-12-12 16:24:25 +0100

    Delete EnigmaCrack directory

--- EnigmaCrack/EnigmaMachine/reflector.py
+++
@@ -1,8 +0,0 @@
-class Reflector:
-    
-    def __init__(self, permutations):
-        self.permutations = permutations
-
-    def reflect(self, letter):
-        letter=self.permutations[ord(letter)-ord('A')]
-        return letter
